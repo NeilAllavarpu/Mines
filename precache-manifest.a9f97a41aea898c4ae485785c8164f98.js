cdf9f4902944e3ff9a246d556a717f77799acf85
Neil Allavarpu <allavarpuneil@gmail.com>
2018-11-25 15:39:32 -0800

    Updates

---
+++ precache-manifest.a9f97a41aea898c4ae485785c8164f98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "480b0f9f31bb0b96a4df",
+    "url": "/Mines/static/css/main.2da4729a.chunk.css"
+  },
+  {
+    "revision": "480b0f9f31bb0b96a4df",
+    "url": "/Mines/static/js/main.480b0f9f.chunk.js"
+  },
+  {
+    "revision": "3c0cd88361514bc37111",
+    "url": "/Mines/static/js/1.3c0cd883.chunk.js"
+  },
+  {
+    "revision": "063d5f9a4c1081ba87ee",
+    "url": "/Mines/static/js/runtime~main.063d5f9a.js"
+  },
+  {
+    "revision": "6af8441beefbb2f356ffe860cbc35159",
+    "url": "/Mines/index.html"
+  }
+];
